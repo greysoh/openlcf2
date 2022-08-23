e3b6c43ab5962aacd3160bd5f6df30b94146cdfd
greysoh <greysoh@pm.me>
2022-08-23 18:45:19 -0400

    chore: Formats code.

--- startLunar.js
+++ startLunar.js
@@ -37,18 +37,30 @@ async function findCopyFiles(version, isIchor) {
     if (
       (multiverData.name.startsWith("lunar") ||
         multiverData.name.startsWith("lunar-") ||
-        multiverData.name.startsWith("common-") || multiverData.name.startsWith("genesis")) &&
+        multiverData.name.startsWith("common-") ||
+        multiverData.name.startsWith("genesis")) &&
       multiverData.name.endsWith(".jar")
     ) {
       data.push(await joinPath(multiverRoot, multiverData.name));
-    } else if (multiverData.name.toLowerCase().startsWith("optifine") && multiverData.name.endsWith(".jar")) {
-      const verData = multiverData.name.startsWith("OptiFine") ? multiverData.name.split("v")[1].split(".")[0].replaceAll("_", ".") : version;
+    } else if (
+      multiverData.name.toLowerCase().startsWith("optifine") &&
+      multiverData.name.endsWith(".jar")
+    ) {
+      const verData = multiverData.name.startsWith("OptiFine")
+        ? multiverData.name.split("v")[1].split(".")[0].replaceAll("_", ".")
+        : version;
 
       if (version.startsWith(verData)) {
         data.push(await joinPath(multiverRoot, multiverData.name));
       }
-    } else if (multiverData.name.startsWith("v") && multiverData.name.endsWith(".jar")) {
-      const verData = multiverData.name.split("v")[1].split("-")[0].replaceAll("_", ".");
+    } else if (
+      multiverData.name.startsWith("v") &&
+      multiverData.name.endsWith(".jar")
+    ) {
+      const verData = multiverData.name
+        .split("v")[1]
+        .split("-")[0]
+        .replaceAll("_", ".");
 
       if (version.startsWith(verData)) {
         data.push(await joinPath(multiverRoot, multiverData.name));
@@ -74,22 +86,35 @@ export async function loadLunarCommand(version, jreArgs, lunarArgs, rootDir) {
   cmd += `${jre} --add-modules jdk.naming.dns --add-exports jdk.naming.dns/com.sun.jndi.dns=java.naming`;
   cmd += ` -Djna.boot.library.path=${nativesDir}`;
   cmd += ` -Dlog4j2.formatMsgNoLookups=true`; // Disable lookups for log4j so that computer no go boom
-  cmd += 
   cmd += ` --add-opens java.base/java.io=ALL-UNNAMED${
     jreArgs ? " " + jreArgs : ""
   }`;
   cmd += ` -Djava.library.path=${nativesDir}`;
   cmd += ` -XX:+DisableAttachMechanism -cp`;
 
-  cmd += ` ${await findCopyFiles(version)} com.moonsworth.lunar.genesis.Genesis`;
+  cmd += ` ${await findCopyFiles(
+    version
+  )} com.moonsworth.lunar.genesis.Genesis`;
   cmd += ` --version ${version}`;
   cmd += ` --accessToken 0 --assetIndex ${version} --userProperties {} --gameDir`;
-  cmd += ` ${rootDir ? rootDir : Deno.build.os == "windows" ? await joinPath(dir("home"), "AppData", "Roaming", ".minecraft") : await joinPath(dir("home"), ".minecraft")}`;
-  cmd += ` --texturesDir ${await joinPath(dir("home"), ".lunarclient", "textures")}`;
+  cmd += ` ${
+    rootDir
+      ? rootDir
+      : Deno.build.os == "windows"
+      ? await joinPath(dir("home"), "AppData", "Roaming", ".minecraft")
+      : await joinPath(dir("home"), ".minecraft")
+  }`;
+  cmd += ` --texturesDir ${await joinPath(
+    dir("home"),
+    ".lunarclient",
+    "textures"
+  )}`;
   cmd += ` --ichorClassPath ${await findCopyFiles(version, true)}`;
-  cmd += ` --ichorExternalFiles OptiFine-${version.split(".")[0]}.${version.split(".")[1]}.jar`
+  cmd += ` --ichorExternalFiles OptiFine-${version.split(".")[0]}.${
+    version.split(".")[1]
+  }.jar`;
   cmd += ` --workingDirectory . --classpathDir ${nativesDir}`;
   cmd += lunarArgs ? " " + lunarArgs : "";
-  
+
   return cmd;
 }
